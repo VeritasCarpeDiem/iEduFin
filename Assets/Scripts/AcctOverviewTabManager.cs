a2323b123a5f45a865458169c46c17fe26317ef1
Madeline Tat <m1tat@ucsd.edu>
2022-08-05 12:11:10 -0700

    updated UI and functionality
    added functionality to position card & balance history buttons, added account value UI

--- Assets/Scripts/AcctOverviewTabManager.cs
+++ Assets/Scripts/AcctOverviewTabManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,9 @@ public class AcctOverviewTabManager : MonoBehaviour
 {
     [SerializeField] public GameObject[] tabBodies;
     [SerializeField] public GameObject[] tabs;
+    [SerializeField] public GameObject[] buttons; //refers to buttons that are not tabs
 
+    //Each time a tab is clicked, the content of that tab will be shown and the other tab contents will be hidden
     public void OnTabSwitch(GameObject tabBody)
     {
         tabBody.SetActive(true);
@@ -21,18 +24,36 @@ public class AcctOverviewTabManager : MonoBehaviour
         }
     } 
     
+    //The color of selected tab will change to white, while the other tabs will be grayed out
+    //If the "tab" is a button, then the selected & unselected colors will be different (not white/gray)
     public void ChangeTabColor(GameObject tab)
     {
-        tab.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-        
-        for (int i = 0; i < tabs.Length; i++)
+        if (buttons.Contains(tab))
         {
-            if (tabs[i] != tab)
-            {
-                tabs[i].GetComponent<Image>().color = new Color32(207, 207, 207, 255);
-            }
-            
-        }
+            tab.GetComponent<Image>().color = new Color32(179, 189, 241, 255);
         
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (buttons[i] != tab)
+                {
+                    buttons[i].GetComponent<Image>().color = new Color32(128, 135, 176, 255);
+                }
+            
+            }
+        }
+        else
+        {
+            tab.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        
+            for (int i = 0; i < tabs.Length; i++)
+            {
+                if (tabs[i] != tab)
+                {
+                    tabs[i].GetComponent<Image>().color = new Color32(207, 207, 207, 255);
+                }
+            
+            }
+        }
+
     }
 }
