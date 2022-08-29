8f0cb5fc4300dcb70836c6158f31653d43a4db1c
Zafeer Syed <zsyed@ucsd.edu>
2022-08-28 22:09:33 -0700

    added logout and change player icon

---
+++ Assets/Scripts/IconSelecter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class IconSelecter : MonoBehaviour
+    {
+        [SerializeField] private GameObject racoonIcon;
+        [SerializeField] private GameObject alumniIcon;
+        [SerializeField] private GameObject khoslaIcon;
+        [SerializeField] private PlayerAccountData playerAcc;
+
+        private void Start()
+        {
+            playerAcc = GameObject.FindWithTag("AccountManager").GetComponent<AccountManager>().playerAccount;
+            switch (playerAcc.className)
+            {
+                case "Racoon":
+                    racoonIcon.SetActive(true);
+                    break;
+                case "Alumni":
+                    alumniIcon.SetActive(true);
+                    break;
+                case "Khosla":
+                    khoslaIcon.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LogOutBtn.cs
@@ -0,0 +1,21 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class LogOutBtn : MonoBehaviour
+    {
+        public void DestroyAllGameObjects()
+        {
+            GameObject[] GameObjects = (FindObjectsOfType<GameObject>() as GameObject[]);
+ 
+            for (int i = 0; i < GameObjects.Length; i++)
+            {
+                Destroy(GameObjects[i]);
+            }
+
+            SceneManager.LoadScene("newloginScene");
+        }
+    }
+}
